be15fc0a323b4613f23a9335407294737ae3a980
William Ramirez <williamr@blanclabs.com>
2019-08-29 15:37:29 -0500

    first commit

---
+++ src/test/java/test/tasks/OpenTheApplication.java
@@ -0,0 +1,19 @@
+package test.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Open;
+import net.thucydides.core.annotations.Step;
+import test.ui.GoogleSearchPage;
+
+public class OpenTheApplication implements Task {
+
+    GoogleSearchPage googleSearchPage;
+
+    @Step("Open the application")
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Open.browserOn().the(googleSearchPage)
+        );
+    }
+}
